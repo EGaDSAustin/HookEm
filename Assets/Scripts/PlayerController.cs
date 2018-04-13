74b4392db9eabd0fdb4ad907fd2e69447d01ee9a
imcdo <ian_mcdonald@rocketmail.com>
2018-04-12 21:27:32 -0500

    camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody playerRigidbody;
+    [SerializeField] private Camera camera;
     [SerializeField] private float translationSpeed;
     [SerializeField] private float maxTranslationSpeed;
     [SerializeField] private float rotationSpeed;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     void FixedUpdate ()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        MovePlayer(playerRigidbody.transform.TransformDirection(new Vector3(0, 0, vertical)));
-        TurnPlayer(new Vector3(0, horizontal, 0));
+        MovePlayer(playerRigidbody.transform.TransformDirection(new Vector3(horizontal, 0,vertical)));
+
+        //camera rotation
+        TurnPlayer(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+        TurnCamera(new Vector3(-1 * Input.GetAxis("Mouse Y"),0, 0));
+
+        
     }
 
     private void MovePlayer (Vector3 velocity)
     {
         Vector3 newVelocity = playerRigidbody.velocity + (velocity * translationSpeed);
         playerRigidbody.velocity = Vector3.ClampMagnitude(newVelocity, maxTranslationSpeed);
+
     }
 
     private void TurnPlayer (Vector3 rotation)
     {
         playerRigidbody.rotation *= Quaternion.Euler(rotationSpeed * rotation);
+    }
+
+    private void TurnCamera(Vector3 rotation)
+    {
+        camera.transform.rotation *= Quaternion.Euler(rotation * rotationSpeed);
     }
 }
